b968158b5c6526ab1e915a0ebc0578de57ce2084
rgrohitgupta938 <49370602+rgrohitgupta938@users.noreply.github.com>
2024-04-25 23:43:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,14 @@
-//your code here
+// script.js
+const addBtn = document.getElementById("addTodoBtn");
+const newInput = document.getElementById("newTodoInput");
+const todoList = document.getElementById("todoList");
 
+addBtn.addEventListener("click", () => {
+    const todoText = newInput.value.trim();
+    if (todoText !== "") {
+        const todoItem = document.createElement("li");
+        todoItem.textContent = todoText;
+        todoList.appendChild(todoItem);
+        newInput.value = "";
+    }
+});
